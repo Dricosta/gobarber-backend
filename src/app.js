621e4f112b90806ca13989ee3194f3fa4dca6f0b
Adriano Costa <adrianocosta@Adrianos-MacBook-Air.local>
2019-06-29 15:09:08 -0300

    criando estrutura mvc e create de users

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
-    constructor(){
+    constructor() {
         this.server = express();
 
         this.middlewares();
         this.routes();
     }
 
-    middlewares(){
+    middlewares() {
         this.server.use(express.json());
     }
 
-    routes(){
+    routes() {
         this.server.use(routes);
     }
-
 }
 
 export default new App().server;
