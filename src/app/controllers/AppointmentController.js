06ea7f6a6cce2e140e83912eabc55be0c7b2d72c
Dricosta <adrianoc55@hotmail.com>
2019-07-08 22:46:17 -0300

    Email Config - SendEmail and Template HTML

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointments';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
@@ -110,7 +111,20 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                },
+            ],
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -129,6 +143,21 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM', às' H:mm'h'",
+                    { locale: pt }
+                ),
+            },
+        });
 
         return res.json(appointment);
     }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '00db3f84f1ef5b',
+        pass: '4b234b4a1aa928',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
